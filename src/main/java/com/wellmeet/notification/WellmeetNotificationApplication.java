d182b387eac3ba954fe98f46bf3538bb2e54def2
unifolio0 <boho82ha@gmail.com>
2025-08-10 16:21:02 +0900

    feat: 기본 세팅

---
+++ src/main/java/com/wellmeet/notification/WellmeetNotificationApplication.java
@@ -0,0 +1,12 @@
+package com.wellmeet.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellmeetNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WellmeetNotificationApplication.class, args);
+    }
+}
